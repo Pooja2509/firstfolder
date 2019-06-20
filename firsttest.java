679cd037eb6cbb783e0e4a9e5783e121d3e02f04
Pooja2509 <45913513+Pooja2509@users.noreply.github.com>
2019-06-20 11:05:17 +0530

    Add files via upload

---
+++ firsttest.java
@@ -0,0 +1,8 @@
+import java.util*;
+import java.util.Scanner;
+public class firsttest{
+public static void main(String[]args){
+Scanner sc=new Scanner(system.in);
+int a=5,b=9;
+system.out.println(a+b);
+}}
